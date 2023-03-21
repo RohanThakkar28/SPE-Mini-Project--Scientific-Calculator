08247f5b9d7ccae98b810dfc19258bc0e7a22f94
RohanThakkar28 <rohanthakkar2801@gmail.com>
2023-03-21 19:25:31 +0530

    logging

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -1,6 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import java.util.Scanner;
 
 public class Functions {
+    public static final Logger logger = LogManager.getLogger(Functions.class);
     public int factorial(int n){
         if(n==1 || n==0){
             return 1;
@@ -13,7 +17,9 @@ public class Functions {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter number");
         int n=sc.nextInt();
+        logger.info("[FACTORIAL] - " + n);
         int x=functions.factorial(n);
+        logger.info("[RESULT - FACTORIAL] - " + x);
         System.out.println(x);
 
     }
